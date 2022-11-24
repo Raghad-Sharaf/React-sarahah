94de84cb26067d5cb3b6cf9d73eff0bac568341a
Raghad-sharaf <raghad.s.sharaf@gmail.com>
2022-11-25 00:08:12 +0200

    register and login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Joi from 'joi';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   let [user,setUser]= useState({
-    
     email:"",
     password:""
   })
@@ -12,30 +13,60 @@ function Login() {
   let getUser = (e)=>{
    setUser({...user,[e.target.name]:e.target.value});
   }
-
-  let  sendData= async(e)=>{
+  let navigate = useNavigate();
+  function goToHome(){
+    let path = '/';
+    navigate (path);
+  }
+  let [errorList,setErrorList] = useState([]);
+  let [errorMsg,setErrorMsg] = useState('');
+  async function submitFormData(e){
     e.preventDefault();
     let {data} = await axios.post("http://localhost:3003/api/v1/auth/signin",user);
-    console.log(data.message);
+    if (data.message == 'login')
+    {
+     goToHome();
+    }else{
+      setErrorMsg(data.message)
+    }
+    let validateResult = validateForm();
+    if (validateResult.error){
+      setErrorList(validateResult.error.details);
+    }
+    console.log(validateResult)
+  }
 
+  function validateForm(){
+    const schema = Joi.object({
+      email:Joi.string().email().required(),
+      password:Joi.string().required().pattern(new RegExp('^[a-z][0,9]{3}$'))
+    })
+    return schema.validate(user,{abortEarly:false})
   }
   return (
     <div className="container text-center my-5">
-  <div className="user my-3">
-    <i className="fas fa-user-secret user-icon" />
-    <h4 className="login">Login</h4>
+    <div className="user my-3">
+      <i className="fas fa-user-secret user-icon" />
+      <h4 className="login">Login</h4>
+    </div>
+    {errorMsg?<div className='alert alert-danger w-50 m-auto'>{errorMsg}</div>:''}
+    
+    <div className="card p-5 w-50 m-auto">
+      {errorList.map((error,index)=> <div key={index} 
+      className="alert alert-danger">
+        {error.message}
+      </div>)}
+      <form method="POST" action="/handleLogin" onSubmit={submitFormData}>
+        <input onChange={getUser} className="form-control my-4" placeholder="Enter your email" type="text" name="email" />
+        <input onChange={getUser} className="form-control my-4 " placeholder="Enter your Password" type="text" name="password" />
+        <button className="btn btn-default-outline" type='submit'>Login</button>
+        <div>
+        <p><Link className="text-muted forgot btn" to="">I Forgot My Password</Link></p>
+        <Link className="btn btn-default-outline" to="/register">Register</Link>
+        </div>
+      </form>
+    </div>
   </div>
-  <div className="card p-5 w-50 m-auto">
-    <form method="POST" action="/handleLogin" onSubmit={sendData}>
-      <input className="form-control" placeholder="Enter your email" type="text" name="email" />
-      <input className="form-control my-4 " placeholder="Enter your Password" type="text" name="password" />
-      <button className="btn btn-default-outline my-4 w-100 rounded" type='submit' to="home">Login</button>
-      <p><Link className="text-muted forgot btn" to="">I Forgot My Password</Link></p>
-      <Link className="btn btn-default-outline" to="register">Register</Link>
-    </form>
-  </div>
-</div>
-
   )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Joi from 'joi';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
   let [user,setUser]= useState({
@@ -13,12 +15,37 @@ function Register() {
   let getUser = (e)=>{
    setUser({...user,[e.target.name]:e.target.value});
   }
-
-  let  sendData= async(e)=>{
+  let navigate = useNavigate();
+  function goToLogin(){
+    let path = '/login';
+    navigate (path);
+  }
+  let [errorList,setErrorList] = useState([]);
+  let [errorMsg,setErrorMsg] = useState('');
+  async function submitFormData(e){
     e.preventDefault();
     let {data} = await axios.post("http://localhost:3003/api/v1/auth/signup",user);
-    console.log(data.message);
+    if (data.message == 'done')
+    {
+     goToLogin();
+    }else{
+      setErrorMsg(data.message)
+    }
+    let validateResult = validateForm();
+    if (validateResult.error){
+      setErrorList(validateResult.error.details);
+    }
+    console.log(validateResult)
+  }
 
+  function validateForm(){
+    const schema = Joi.object({
+      userName:Joi.string().required().min(3).max(10),
+      email:Joi.string().email().required(),
+      password:Joi.string().required().pattern(new RegExp('^[a-z][0,9]{3}$')),
+      cpassword:Joi.string().valid(Joi.ref('password')).required()
+    })
+    return schema.validate(user,{abortEarly:false})
   }
   return (
     <div className="container text-center my-5">
@@ -26,8 +53,14 @@ function Register() {
       <i className="fas fa-user-secret user-icon" />
       <h4 className="login">Register</h4>
     </div>
+    {errorMsg?<div className='alert alert-danger w-50 m-auto'>{errorMsg}</div>:''}
+    
     <div className="card p-5 w-50 m-auto">
-      <form method="POST" action="/handleLogin" onSubmit={sendData}>
+      {errorList.map((error,index)=> <div key={index} 
+      className="alert alert-danger">
+        {error.message}
+      </div>)}
+      <form method="POST" action="/handleLogin" onSubmit={submitFormData}>
         <input onChange={getUser} className="form-control" placeholder="Enter your name" type="text" name="userName" />
         <input onChange={getUser} className="form-control my-4" placeholder="Enter your email" type="text" name="email" />
         <input onChange={getUser} className="form-control my-4 " placeholder="Enter your Password" type="text" name="password" />
